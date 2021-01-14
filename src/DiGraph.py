27e1052c50097cb384b818380fbe5a07f8b097d8
barakamram <74139212+barakamram@users.noreply.github.com>
2021-01-14 03:03:53 +0200

    Add files via upload

---
+++ src/DiGraph.py
@@ -0,0 +1,243 @@
+import copy
+import random
+
+from src.GraphInterface import GraphInterface
+
+
+class Node:
+
+    def __init__(self, key: int, pos: tuple = None):
+        self.key = key
+        self.pos = pos
+        self.weight = 0.0
+        self.info = ""
+        self.parent = None
+        self.hashIn = {}
+        self.hashOut = {}
+        if pos is None:
+            x = random.uniform(35.001, 35.999)
+            y = random.uniform(32.001, 32.999)
+            self.pos = (x, y, 0.0)
+
+    def get_key(self) -> int:
+        return self.key
+
+    def get_pos(self):
+        return self.pos
+
+    def set_pos(self, p):
+        self.pos = p
+
+    def get_weight(self) -> float:
+        return self.weight
+
+    def set_weight(self, w: float):
+        self.weight = w
+
+    def get_info(self) -> str:
+        return self.info
+
+    def set_info(self, s: str):
+        self.info = s
+
+    def get_parent(self):
+        return self.parent
+
+    def set_parent(self, node_id: int):
+        self.parent = node_id
+
+    def get_hashIn(self) -> dict:
+        return self.hashIn
+
+    def add_hashIn(self, node_id: int, weight: float) -> bool:
+        if self.key is not node_id:
+            if self.hashIn.get(node_id):
+                if self.hashIn.get(node_id) != weight:
+                    self.hashIn[node_id] = weight
+                    return True
+            self.hashIn.setdefault(node_id, weight)
+        return False
+
+    def remove_hashIn(self, node_id: int) -> bool:
+        if self.hashIn.get(node_id):
+            del self.hashIn[node_id]
+            return True
+        return False
+
+    def get_hashOut(self) -> dict:
+        return self.hashOut
+
+    def add_hashOut(self, node_id: int, weight: float) -> bool:
+        if self.key is not node_id:
+            if self.hashOut.get(node_id):
+                if self.hashOut.get(node_id) != weight:
+                    self.hashOut[node_id] = weight
+                    return True
+            self.hashOut.setdefault(node_id, weight)
+        return False
+
+    def remove_hashOut(self, node_id: int) -> bool:
+        if self.hashOut.get(node_id):
+            del self.hashOut[node_id]
+            return True
+        return False
+
+    def __eq__(self, other):
+        if self is None:
+            return False
+        if other is None or other.__class__ != self.__class__:
+            return False
+        return self.get_hashIn().keys().__eq__(other.get_hashIn().keys()) and self.get_hashOut().keys().__eq__(
+            other.get_hashOut().keys())
+
+    def __str__(self):
+        return self.key
+
+    def __repr__(self):
+        return repr((self.key, self.pos))
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+
+class DiGraph(GraphInterface):
+    """This abstract class represents a directed weighted graph."""
+
+    def __init__(self):
+        self.nodes = {}
+        self.num_of_edges = 0
+        self.mc = 0
+
+    def get_node(self, node_id: int) -> Node:
+        return self.nodes.get(node_id)
+
+    def get_edge(self, id1: int, id2: int) -> float:
+        if not self.has_edge(id1, id2):
+            return -1
+        return self.get_node(id1).get_hashOut().get(id2)
+
+    def v_size(self) -> int:
+        """
+        Returns the number of vertices in this graph
+        @return: The number of vertices in this graph
+        """
+        if self is None:
+            return 0
+        return len(self.nodes)
+
+    def e_size(self) -> int:
+        """
+        Returns the number of edges in this graph
+        @return: The number of edges in this graph
+        """
+        if self is None:
+            return 0
+        return self.num_of_edges
+
+    def get_all_v(self) -> dict:
+        """
+        return a dictionary of all the nodes in the Graph, each node is represented using a pair
+        (node_id, node_data)
+        """
+        return self.nodes
+
+    def all_in_edges_of_node(self, id1: int) -> dict:
+        """
+        return a dictionary of all the nodes connected to (into) node_id ,
+        each node is represented using a pair (other_node_id, weight)
+        """
+        return self.get_node(id1).get_hashIn()
+
+    def all_out_edges_of_node(self, id1: int) -> dict:
+        """
+        return a dictionary of all the nodes connected from node_id , each node is represented using a pair
+        (other_node_id, weight)
+        """
+        return self.get_node(id1).get_hashOut()
+
+    def get_mc(self) -> int:
+        """
+        Returns the current version of this graph,
+        on every change in the graph state - the MC should be increased
+        @return: The current version of this graph.
+        """
+        return self.mc
+
+    def add_node(self, node_id: int, pos: tuple = None) -> bool:
+        """
+        Adds a node to the graph.
+        @param node_id: The node ID
+        @param pos: The position of the node
+        @return: True if the node was added successfully, False o.w.
+
+        Note: if the node id already exists the node will not be added
+        """
+        if not self.nodes.__contains__(node_id):
+            node = Node(node_id, pos)
+            self.nodes[node_id] = node
+            self.mc += 1
+            return True
+        return False
+
+    def remove_node(self, node_id: int) -> bool:
+        """
+        Removes a node from the graph.
+        @param node_id: The node ID
+        @return: True if the node was removed successfully, False o.w.
+
+        Note: if the node id does not exists the function will do nothing
+        """
+        if self.nodes.__contains__(node_id):
+            hash_in = self.get_node(node_id).get_hashIn()
+            hash_out = self.get_node(node_id).get_hashOut()
+            for node in hash_in.keys():
+                self.get_node(node).remove_hashOut(node_id)
+                self.num_of_edges -= 1
+            for node in hash_out.keys():
+                self.get_node(node).remove_hashIn(node_id)
+                self.num_of_edges -= 1
+            self.nodes.pop(node_id)
+            self.mc += 1
+            return True
+        return False
+
+    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+        """
+        Adds an edge to the graph.
+        @param id1: The start node of the edge
+        @param id2: The end node of the edge
+        @param weight: The weight of the edge
+        @return: True if the edge was added successfully, False o.w.
+
+        Note: If the edge already exists or one of the nodes dose not exists the functions will do nothing
+        """
+        if self.nodes.__contains__(id1) and self.nodes.__contains__(id2):
+            if not self.has_edge(id1, id2) and id1 != id2:
+                self.get_node(id1).add_hashOut(id2, weight)
+                self.get_node(id2).add_hashIn(id1, weight)
+                self.num_of_edges += 1
+                self.mc += 1
+                return True
+        return False
+
+    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+        """
+        Removes an edge from the graph.
+        @param node_id1: The start node of the edge
+        @param node_id2: The end node of the edge
+        @return: True if the edge was removed successfully, False o.w.
+
+        Note: If such an edge does not exists the function will do nothing
+        """
+        if self.nodes.__contains__(node_id1) and self.nodes.__contains__(node_id2):
+            if self.has_edge(node_id1, node_id2):
+                self.get_node(node_id1).remove_hashOut(node_id2)
+                self.get_node(node_id2).remove_hashIn(node_id1)
+                self.num_of_edges -= 1
+                self.mc += 1
+                return True
+        return False
+
+    def has_edge(self, id1: int, id2: int) -> bool:
+        return not self.get_node(id1).get_hashOut().get(id2) is None
+
---
+++ src/GraphAlgo.py
@@ -0,0 +1,206 @@
+import json
+from typing import List
+
+from queue import PriorityQueue
+
+from matplotlib.patches import ConnectionPatch
+
+from src.DiGraph import DiGraph
+from src.GraphAlgoInterface import GraphAlgoInterface
+from src.GraphInterface import GraphInterface
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+class GraphAlgo(GraphAlgoInterface):
+    """This abstract class represents the algorithms of a graph."""
+
+    def __init__(self, graph: DiGraph = None):
+        if graph is None:
+            self.graph = DiGraph()
+        self.graph_algo = graph
+
+    def get_graph(self) -> GraphInterface:
+        """
+        :return: the directed graph on which the algorithm works on.
+        """
+        return self.graph_algo
+
+    def load_from_json(self, file_name: str) -> bool:
+        """
+        Loads a graph from a json file.
+        @param file_name: The path to the json file
+        @returns True if the loading was successful, False o.w.
+        """
+        try:
+            with open(file_name, "r") as file:
+                graph = DiGraph()
+                load = json.load(file)
+                for node in load.get("Nodes"):
+                    key = node.get("id")
+                    pos = None
+                    if node.get("pos") is not None:
+                        lp = node.get("pos").split(",")
+                        x = lp[0]
+                        y = lp[1]
+                        z = lp[2]
+                        pos = (x, y, z)
+                    graph.add_node(key, pos)
+                for edge in load.get("Edges"):
+                    graph.add_edge(edge.get("src"), edge.get("dest"), edge.get("w"))
+            self.graph_algo = graph
+            return True
+        except IOError as e:
+            print(e)
+            return False
+
+    def save_to_json(self, file_name: str) -> bool:
+        """
+        Saves the graph in JSON format to a file
+        @param file_name: The path to the out file
+        @return: True if the save was successful, False o.w.
+        """
+        Nodes = []
+        Edges = []
+        for node in self.graph_algo.get_all_v():
+            nodes = {"id": node}
+            Nodes.append(nodes)
+            node_out = self.graph_algo.all_out_edges_of_node(node)
+            edges_keys = self.graph_algo.all_out_edges_of_node(node).keys()
+            for dest in edges_keys:
+                edges = {"src": node, "dest": dest, "w": node_out.get(dest)}
+                Edges.append(edges)
+        ans = {"Nodes": Nodes, "Edges": Edges}
+        try:
+            with open(file_name, "w") as file:
+                json.dump(ans, file)
+                return True
+        except IOError as e:
+            print(e)
+            return False
+
+    def shortest_path(self, id1: int, id2: int) -> (float, list):
+        """
+        Returns the shortest path from node id1 to node id2 using Dijkstra's Algorithm
+        @param id1: The start node id
+        @param id2: The end node id
+        @return: The distance of the path, a list of the nodes ids that the path goes through
+
+        Notes:
+        If there is no path between id1 and id2, or one of them dose not exist the function returns (float('inf'),[])
+        More info:
+        https://en.wikipedia.org/wiki/Dijkstra's_algorithm
+        """
+        path_list = []
+        path_size = -1
+        node2 = self.graph_algo.get_node(id2)
+        nodes = self.graph_algo.get_all_v()
+        if not nodes.__contains__(id1) or not nodes.__contains__(id2):
+            return path_size, path_list
+        if id1 == id2:
+            path_size = 0
+            path_list.append(id1)
+            return path_size, path_list
+
+        self.dijkstra(id1, id2)
+        path_size = node2.get_weight()
+        if path_size != -1:
+            path_list.insert(0, id2)
+            prev_node = node2.get_parent()
+            while prev_node != id1:
+                path_list.insert(0, prev_node)
+                prev_node = self.graph_algo.get_node(prev_node).get_parent()
+            path_list.insert(0, id1)
+        return path_size, path_list
+
+    def connected_component(self, id1: int) -> list:
+        """
+        Finds the Strongly Connected Component(SCC) that node id1 is a part of.
+        @param id1: The node id
+        @return: The list of nodes in the SCC
+
+        Notes:
+        If the graph is None or id1 is not in the graph, the function should return an empty list []
+        """
+        _list = []
+        if self.graph_algo.get_all_v().__contains__(id1):
+            edges = self.graph_algo.all_out_edges_of_node(id1)
+            for node_id in edges:
+                if self.graph_algo.all_out_edges_of_node(node_id).__contains__(id1):
+                    _list.append(node_id)
+        return _list
+
+    def connected_components(self) -> List[list]:
+        """
+        Finds all the Strongly Connected Component(SCC) in the graph.
+        @return: The list all SCC
+
+        Notes:
+        If the graph is None the function should return an empty list []
+        """
+        self.reset_nodes()
+        _list = []
+        for node_id in self.graph_algo.get_all_v().keys():
+            if self.graph_algo.get_node(node_id).get_info() != "grey":
+                list_of_scc = self.connected_component(node_id)
+                self.graph_algo.get_node(node_id).set_info("grey")
+                for key in list_of_scc:
+                    node = self.graph_algo.get_node(key)
+                    if node.get_info() != "grey":
+                        _list.append([node_id, key])
+        return _list
+
+    def plot_graph(self) -> None:
+        """
+        Plots the graph.
+        If the nodes have a position, the nodes will be placed there.
+        Otherwise, they will be placed in a random but elegant manner.
+        @return: None
+        """
+        fig, ax = plt.subplots()
+        for key in self.graph_algo.get_all_v().keys():
+            node = self.graph_algo.get_node(key)
+            x, y, z = node.get_pos()
+            src = np.array([x, y])
+            ax.annotate(node.get_key(), (x, y), color='black')
+            for edge in self.graph_algo.all_out_edges_of_node(key).keys():
+                x, y, z = self.graph_algo.get_node(edge).get_pos()
+                dest = np.array([x, y])
+                ax.plot([src[0], dest[0]], [src[1], dest[1]], "o", color='red')
+                ax.add_artist(ConnectionPatch(src, dest, "data", "data", shrinkA=5, shrinkB=5, arrowstyle="->", color="grey"))
+        plt.title("Graph")
+        plt.show()
+
+    def reset_nodes(self):
+        nodes = self.graph_algo.get_all_v()
+        for key in nodes.keys():
+            node = self.graph_algo.get_node(key)
+            node.set_weight(-1)
+            node.set_parent(-1)
+            node.set_info("white")
+
+    def dijkstra(self, src: int, dest: int):
+        queue = PriorityQueue()
+        self.reset_nodes()
+        queue.put(src)
+        current = queue.get()
+        queue.put(src)
+        self.graph_algo.get_node(src).set_weight(0)
+        while not queue.empty() and current != dest:
+            current = queue.get()
+            curr_node = self.graph_algo.get_node(current)
+            curr_node.set_info("black")
+            for neighbor in self.graph_algo.all_out_edges_of_node(current).keys():
+                next_node = self.graph_algo.get_node(neighbor)
+                wei = self.graph_algo.get_edge(current, neighbor)
+                if next_node.get_info() != "black":
+                    queue.put(next_node.get_key())
+                    if next_node.get_weight() > curr_node.get_weight() + wei or next_node.get_weight() == -1:
+                        next_node.set_weight(curr_node.get_weight() + wei)
+                        next_node.set_parent(curr_node.get_key())
+
+                if next_node.get_weight() > curr_node.get_weight() + wei:
+                    next_node.set_weight(curr_node.get_weight() + wei)
+                    next_node.set_parent(curr_node.get_key())
+                    queue.put(next_node.get_key())
+
